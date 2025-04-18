4a1cdd31d089926f7fb6c0904b50eb1be472a10d
Kevin Ma <kma315@gatech.edu>
2025-04-18 13:33:20 -0400

    fix: update JWT parsing to be compatible with version 0.12.3

--- backend/src/main/java/com/reptrack/api/security/config/JwtService.java
+++ backend/src/main/java/com/reptrack/api/security/config/JwtService.java
@@ -62,7 +62,7 @@ public class JwtService {
 
     private Claims extractAllClaims(String token) {
         return Jwts
-                .parserBuilder()
+                .parser()
                 .setSigningKey(getSignInKey())
                 .build()
                 .parseClaimsJws(token)
